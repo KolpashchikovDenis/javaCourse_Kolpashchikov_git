64970da943f100183957b69af2ab2fc4e6e77613
KolpashchikovDenis <kdv-78@yandex.ru>
2025-03-26 17:04:07 +0400

    Добавлен простой вывод

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
 
+    public static String name = "Денис Викторович";
+    public static int age = 47;
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.printf("Приветствую, %s. Тебе %d лет.\n", name, age);
     }
 }
