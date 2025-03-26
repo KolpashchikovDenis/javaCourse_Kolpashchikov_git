8967bbdaa45ccf733238c28f6c5e4b83caf03b8b
KolpashchikovDenis <kdv-78@yandex.ru>
2025-03-26 17:13:33 +0400

    Воод имени и возраста с клавиатуры

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static String name = "Денис Викторович";
     public static int age = 47;
 
     public static void main(String[] args) {
+        System.out.println("Введи имя возраст");
+        Scanner scanner = new Scanner(System.in);
+        name = scanner.nextLine();
+        age = scanner.nextInt();
         System.out.printf("Приветствую, %s. Тебе %d лет.\n", name, age);
     }
 }
